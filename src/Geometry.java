a09a926b5ac83eb4ff51970af0a98b9b8f8dc07f
gennabarge <gennabarge@172.16.21.168>
2018-11-20 09:16:13 -0500

    Unit 2 Quiz 1 programming code

--- src/Geometry.java
+++ src/Geometry.java
@@ -5,7 +5,7 @@ public class Geometry {
 		
 		Rectangle rect1 = new Rectangle();
 		Rectangle rect2 = new Rectangle(12.5, 11);
-		rect2.setHeight(-10);
+		rect2.setHeight(-11);
 		System.out.println("Rectangle 1: " + rect1 + " has an area of " + rect1.getArea() + " sq ft");
 		System.out.println("Rectangle 2: " + rect2 + " has an area of " + rect2.getArea() + " sq ft");
 
--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,57 @@
 
 public class Rectangle {
+	private double width;
+	private double height; 
+	private double area; 
+	
+	public Rectangle() {
+		width = 10;
+		height = 10;
+		setArea(10,10); 
+	}
+	public Rectangle(double width, double height) {
+		setWidth(width);
+		setHeight(height); 
+		setArea(width, height); 
+	}
+	public void setWidth(double w) {
+		if (w > 0) {
+			width = w; 
+		}
+		else {
+			width = 10;
+			System.out.println("Your width can't be negative/zero! Defaulting to 10");
+		}
+		setArea(width, height);
+	}
+	public void setHeight(double h) {
+		if (h > 0) {
+			height = h; 
+		}
+		else {
+			height = 10;
+			System.out.println("Your height can't be negative/zero! Defaulting to 10");
+		}
+		setArea(width, height);
+	}
+	public double getWidth() {
+		return width; 
+	}
+	public double getHeight() {
+		return height; 
+	}
+	public void setArea(double height, double width) {
+		area = height * width;
+	}
+	public double getArea() {
+		return area; 
+	}
+	
+	public String toString() {
+		return "The rectangle's dimensions are:" + width + "ft by " + height +
+				"ft"; 
+	}
+	
+	}
 
-}
+
